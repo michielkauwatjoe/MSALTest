ce7f3540ba7997769a03b37f89caf669e6a2c650
Michiel <michiel@kauwatjoe.com>
2021-01-27 16:45:34 +0100

    Adding authentication functions.

--- django/graph_tutorial/tutorial/urls.py
+++ django/graph_tutorial/tutorial/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-  # /
-  path('', views.home, name='home'),
-  # TEMPORARY
-  path('signin', views.home, name='signin'),
-  path('signout', views.home, name='signout'),
-  path('calendar', views.home, name='calendar'),
+    # /
+    path('', views.home, name='home'),
+    path('signin', views.sign_in, name='signin'),
+    path('signout', views.sign_out, name='signout'),
+    path('calendar', views.home, name='calendar'),
+    path('callback', views.callback, name='callback'),
 ]
